febebae3fb0072f70f43bc568110beb05cebd73d
AnilBamne <anilbamne20@gmail.com>
2022-12-01 07:40:29 +0530

    Adding UC1CreatingStackUseingPushMethod code

---
+++ StackAndQueUsingLinkedList222Batch/CustomisedStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StackAndQueUsingLinkedList222Batch
+{
+    public class CustomisedStack
+    {
+        private Node top;
+        public CustomisedStack()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            Console.WriteLine("Values in stack are : ");
+            while (temp != null)
+            {
+                Console.Write(temp.data + " "); //56 30 70 
+                temp = temp.next;
+            }
+        }
+    }
+}
--- StackAndQueUsingLinkedList222Batch/Program.cs
+++ StackAndQueUsingLinkedList222Batch/Program.cs
@@ -7,6 +7,26 @@ namespace StackAndQueUsingLinkedList222Batch
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack and Queue");
+            Console.WriteLine("Choose the below option to execute the program\n1:Create a stack of 56->30->70\n2:Peek and Pop from stack\n3:Create a Queue of 56->30->70\n4:Dequeue from begining\n");
+            int option=int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    CustomisedStack cstack=new CustomisedStack();
+                    cstack.Push(70);
+                    cstack.Push(30);
+                    cstack.Push(56);
+                    cstack.Display();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
